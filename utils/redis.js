d0a636e9a0031261514dc1a88597e5694b511e30
Irafasha Jules <jirafasha@ubuntud.myguest.virtualbox.org>
2024-05-12 22:15:06 +0200

    TASK00 UPDATE01

--- utils/redis.js
+++ utils/redis.js
@@ -1,52 +1,48 @@
 import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
     this.client = createClient();
+    this.isClientConnected = true;
 
     this.client.on('error', (error) => {
       console.error('Redis Client Error:', error);
+      this.isClientConnected = false;
     });
+
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setexAsync = promisify(this.client.setex).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
   }
 
   isAlive() {
-    return this.client.connected = true;
+    return this.isClientConnected;
   }
 
   async get(key) {
-    return new Promise((resolve, reject) => {
-      this.client.get(key, (error, reply) => {
-        if (error) {
-          reject(error);
-          return;
-        }
-        resolve(reply);
-      });
-    });
+    try {
+      return await this.getAsync(key);
+    }
+    catch (error) {
+      throw new Error('Error reading from redis:', error);
+    }
   }
 
   async set(key, value, duration) {
-    return new Promise((resolve, reject) => {
-      this.client.setex(key, duration, value, (error, reply) => {
-        if (error) {
-          reject(error);
-          return;
-        }
-        resolve(reply);
-      });
-    });
+    try {
+      return await this.setexAsync(key, duration, value);
+    } catch (error) {
+      throw new Error('Error writting to redis:', error);
+    }
   }
 
   async del(key) {
-    return new Promise((resolve, reject) => {
-      this.client.del(key, (error, reply) => {
-        if (error) {
-          reject(error);
-          return;
-        }
-        resolve(reply);
-      });
-    });
+    try {
+      return await this.delAsync(key);
+    } catch (error) {
+      throw new Error('Error deleting from Redis:', error);
+    }
   }
 }
 
